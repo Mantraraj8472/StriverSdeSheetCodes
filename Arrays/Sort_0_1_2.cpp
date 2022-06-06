373a7769be6aaa0e1cdd9e0c3bf596cb9137d897
Mantra3265 <mantrarajgotecha@gmail.com>
2022-06-06 21:17:44 +0530

    Added Sort_0_1_2.cpp with 2 pointer approach

---
+++ Arrays/Sort_0_1_2.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h> 
+void sort012(int *arr, int n)
+{
+    int l = 0, r = n - 1;
+    for(int i = 0; i < n; i++){
+        // Why this loop or why not loop for l?
+        // Because we are damn sure that for all i < l arr[i] == 0 but we are not sure that arr[r] == 2 or not.
+        while(r >= 0 and arr[r] == 2) r--;
+        if(i == r + 1) break;
+        if(arr[i] == 2){
+            swap(arr[i], arr[r]);
+            r--;
+        }
+        if(arr[i] == 0) {
+            swap(arr[i], arr[l]);
+            l++;
+        }
+    }
+}
