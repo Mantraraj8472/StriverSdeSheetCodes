0147f0ab204180d5e920b9e2efd6e3796ae5ac28
Mantra3265 <mantrarajgotecha@gmail.com>
2022-06-08 15:50:44 +0530

    Added Iterative approach in  Modular_Exponentiation.cpp

--- Arrays/Modular_Exponentiation.cpp
+++ Arrays/Modular_Exponentiation.cpp
@@ -10,6 +10,18 @@ long long power(long long a, long long b, long long mod){
 }
 
 int modularExponentiation(int x, int n, int m) {
-	long long ans = power(x, n, m);
-    return (int)ans;
+// 	long long ans = power(x, n, m);
+//     return (int)ans;
+    
+    // Above method has time complexity of O(logb) and also space complexity of O(logb) which is a stack space.
+    // So by using iterative approach we can eleminate this stack spae
+    long long ans = 1ll;
+    long long xc = x;
+    while(n){
+        if(n & 1)
+            ans = (1ll * ans * xc) % m;
+        n >>= 1;
+        xc = (1ll * xc * xc) % m;
+    }
+    return ans;
 }
