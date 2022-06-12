b8983d0dc544864efd2dce363663f59461ff413f
Mantra3265 <mantrarajgotecha@gmail.com>
2022-06-12 16:30:44 +0530

    Added Iterative approach in Delete_Kth_node_From_End.cpp

--- Linked_List/MustDoSecondTime/Delete_Kth_node_From_End.cpp
+++ Linked_List/MustDoSecondTime/Delete_Kth_node_From_End.cpp
@@ -18,6 +18,9 @@
 
 *****************************************************************/
 
+// Problem link:
+// https://www.codingninjas.com/codestudio/problems/delete-kth-node-from-end-in-linked-list_799912?topList=striver-sde-sheet-problems&leftPanelTab=1
+
 void deleteK(LinkedListNode<int>* head, int &k){
     if(head == NULL) return;
     deleteK(head->next,k);
@@ -29,10 +32,24 @@ void deleteK(LinkedListNode<int>* head, int &k){
 
 LinkedListNode<int>* removeKthNode(LinkedListNode<int> *head, int K)
 {
-    if(head == NULL)
-        return head;
-    deleteK(head, K);
-    if(K > -1)
-        return head->next;
-    return head;
+//     if(head == NULL)
+//         return head;
+//     deleteK(head, K);
+//     if(K > -1)
+//         return head->next;
+//     return head;
+    
+    // Iterative and time complexity of O(N) and space complexity of O(1)
+    LinkedListNode<int> *start = new LinkedListNode<int>(1e9);
+    start->next = head;
+    LinkedListNode<int> *first = start, *second = start;
+    for(int i = 1; i <= K; i++)
+        second = second->next;
+    while(second->next){
+        first = first->next;
+        second = second->next;
+    }
+    if(first->next)
+        first->next = first->next->next;
+    return start->next;
 }
