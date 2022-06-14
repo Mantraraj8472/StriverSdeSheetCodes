e67cbd303ee2a9eb46c94d9d54c6463262912e71
Mantra3265 <mantrarajgotecha@gmail.com>
2022-06-14 14:28:42 +0530

    Added Intersection_of_Two_Linked_Lists.cpp

---
+++ Linked_List/Intersection_of_Two_Linked_Lists.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h> 
+/****************************************************************
+ 
+    Following is the class structure of the Node class:
+
+        class Node
+        {
+        public:
+	        int data;
+	        Node *next;
+	        Node(int data)
+	        {
+		        this->data = data;
+		        this->next = NULL;
+	        }
+        };
+
+*****************************************************************/
+
+int findIntersection(Node *firstHead, Node *secondHead)
+{
+//     unordered_map<Node*, int> hsh;
+//     while(firstHead){
+//         hsh[firstHead] = 1;
+//         firstHead = firstHead->next;
+//     }
+//     while(secondHead){
+//         if(hsh.find(secondHead) != hsh.end()) return secondHead->data;
+//         secondHead = secondHead->next;
+//     }
+//     return -1;
+    
+    // In space complexity of O(1)
+    int len1 = 0, len2 = 0;
+    Node *h1 = firstHead, *h2 = secondHead;
+    while(h1){
+        len1++;
+        h1 = h1->next;
+    }
+    while(h2){
+        len2++;
+        h2 = h2->next;
+    }
+    if(len1 > len2) {
+        swap(firstHead, secondHead);
+        swap(len1, len2);
+    }
+    int diff = len2 - len1;
+    while(diff){
+        diff--;
+        secondHead = secondHead->next;
+    }
+    while(firstHead and secondHead){
+        if(firstHead == secondHead) return firstHead->data;
+        firstHead = firstHead->next;
+        secondHead = secondHead->next;
+    }
+    return -1;
+}
