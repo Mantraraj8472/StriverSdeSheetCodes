ebb53e37b358094a43cf788bc69babd164a679dc
Mantra3265 <mantrarajgotecha@gmail.com>
2022-06-14 14:51:08 +0530

    Added Difference approach in Intersection_of_Two_Linked_Lists.cpp

--- Linked_List/Intersection_of_Two_Linked_Lists.cpp
+++ Linked_List/Intersection_of_Two_Linked_Lists.cpp
@@ -31,29 +31,40 @@ int findIntersection(Node *firstHead, Node *secondHead)
 //     return -1;
     
     // In space complexity of O(1)
-    int len1 = 0, len2 = 0;
+//     int len1 = 0, len2 = 0;
+//     Node *h1 = firstHead, *h2 = secondHead;
+//     while(h1){
+//         len1++;
+//         h1 = h1->next;
+//     }
+//     while(h2){
+//         len2++;
+//         h2 = h2->next;
+//     }
+//     if(len1 > len2) {
+//         swap(firstHead, secondHead);
+//         swap(len1, len2);
+//     }
+//     int diff = len2 - len1;
+//     cout << diff << endl;
+//     while(diff){
+//         diff--;
+//         secondHead = secondHead->next;
+//     }
+    
+//     while(firstHead and secondHead){
+//         if(firstHead == secondHead) return firstHead->data;
+//         firstHead = firstHead->next;
+//         secondHead = secondHead->next;
+//     }
+//     return -1;
+    
+    // Same concept of difference in length but using different approach
+    
     Node *h1 = firstHead, *h2 = secondHead;
-    while(h1){
-        len1++;
-        h1 = h1->next;
+    while(h1 != h2){
+        h1 = h1 == NULL ? secondHead : h1->next;
+        h2 = h2 == NULL ? firstHead : h2->next;
     }
-    while(h2){
-        len2++;
-        h2 = h2->next;
-    }
-    if(len1 > len2) {
-        swap(firstHead, secondHead);
-        swap(len1, len2);
-    }
-    int diff = len2 - len1;
-    while(diff){
-        diff--;
-        secondHead = secondHead->next;
-    }
-    while(firstHead and secondHead){
-        if(firstHead == secondHead) return firstHead->data;
-        firstHead = firstHead->next;
-        secondHead = secondHead->next;
-    }
-    return -1;
+    return h1 == NULL ? -1 : h1->data;
 }
