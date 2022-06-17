c7f3bb78b8e2c9129bd09e3dc920581f0f14df1f
Mantra3265 <mantrarajgotecha@gmail.com>
2022-06-17 11:40:53 +0530

    Added constant space complexity method in Copy_List_with_Random_Pointer.cpp

--- Linked_List/MustDoSecondTime/Copy_List_with_Random_Pointer.cpp
+++ Linked_List/MustDoSecondTime/Copy_List_with_Random_Pointer.cpp
@@ -21,24 +21,54 @@
 
 LinkedListNode<int> *cloneRandomList(LinkedListNode<int> *head)
 {
-    unordered_map<LinkedListNode<int>*, LinkedListNode<int>*> hsh;
-    LinkedListNode<int> *temp = head, *tempans = NULL, *ans = NULL;
-    hsh[NULL] = NULL;
+//     unordered_map<LinkedListNode<int>*, LinkedListNode<int>*> hsh;
+//     LinkedListNode<int> *temp = head, *tempans = NULL, *ans = NULL;
+//     hsh[NULL] = NULL;
+//     while(temp){
+//         LinkedListNode<int> *node = new LinkedListNode<int>(temp->data);
+//         hsh[temp] = node;
+//         temp = temp->next;
+//     }
+//     temp = head;
+//     while(temp){
+//         if(ans == NULL) {
+//             ans = hsh[temp];
+//             tempans = hsh[temp];
+//         }
+//         tempans->next = hsh[temp->next];
+//         tempans->random = hsh[temp->random];
+//         tempans = tempans->next;
+//         temp = temp->next;
+//     }
+//     return ans;
+    
+    // In constant extra space complexity
+    if(head == NULL) return NULL;
+    LinkedListNode<int> *temp = head;
     while(temp){
         LinkedListNode<int> *node = new LinkedListNode<int>(temp->data);
-        hsh[temp] = node;
-        temp = temp->next;
+        node->next = temp->next;
+        temp->next = node;
+        temp = node->next;
     }
     temp = head;
+    LinkedListNode<int> *ans = temp->next, *copy = temp->next;
     while(temp){
-        if(ans == NULL) {
-            ans = hsh[temp];
-            tempans = hsh[temp];
-        }
-        tempans->next = hsh[temp->next];
-        tempans->random = hsh[temp->random];
-        tempans = tempans->next;
+        if(temp->random)
+            temp->next->random = temp->random->next;
+        else 
+            temp->next->random = NULL;
+        temp = temp->next->next;
+    }
+    temp = head;
+    while(temp and ans){
+        temp->next = temp->next->next;
+        if(ans->next)
+            ans->next = ans->next->next;
+        else
+            ans->next = NULL;
+        ans = ans->next;
         temp = temp->next;
     }
-    return ans;
+    return copy;
 }
