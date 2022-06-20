d5cb0aad45879afbaaff23f2026ea3e98c7580b9
Mantra3265 <mantrarajgotecha@gmail.com>
2022-06-20 13:43:59 +0530

    Added iterative method in Subset_Sum.cpp

--- Recursion/Subset_Sum.cpp
+++ Recursion/Subset_Sum.cpp
@@ -12,9 +12,23 @@ void solve(int ind, vector<int> &num, int sum, vector<int> &ans){
 
 vector<int> subsetSum(vector<int> &num)
 {
+//     vector<int> ans;
+//     int sum = 0;
+//     solve(num.size(), num, sum, ans);
+//     sort(ans.begin(), ans.end());
+//     return ans;
+    
+    // Iterative way
     vector<int> ans;
-    int sum = 0;
-    solve(num.size(), num, sum, ans);
+    int n = num.size();
+    for(int i = 0; i < (1 << n); i++){
+        int sum = 0;
+        for(int j = 0; j < n; j++){
+            if((1 << j) & i) 
+                sum += num[j];
+        }
+        ans.push_back(sum);
+    }
     sort(ans.begin(), ans.end());
     return ans;
 }
