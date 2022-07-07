8e40fc020952317bc1f9591a0ca568c1539b43ed
Mantra3265 <mantrarajgotecha@gmail.com>
2022-07-07 15:45:23 +0530

    Added Single queue method in Stack_Using_Queue.cpp

--- Stack_And_Queue/Stack_Using_Queue.cpp
+++ Stack_And_Queue/Stack_Using_Queue.cpp
@@ -3,17 +3,66 @@
 
 #include <bits/stdc++.h>
 
+// class Stack {
+//     private:
+// 	queue<int> q1, q2;
+
+//    public:
+//     Stack() {
+//         queue<int> empty;
+//         q1 = empty;
+//         q2 = empty;        
+//     }
+
+//     /*----------------- Public Functions of Stack -----------------*/
+
+//     int getSize() {
+//         return q1.size();
+//     }
+
+//     bool isEmpty() {
+//         return q1.size() == 0;
+//     }
+
+//     void push(int element) {
+//         if(q1.size() == 0){
+//             q1.push(element);
+//             return;
+//         }
+//         while(q1.size()){
+//             q2.push(q1.front());
+//             q1.pop();
+//         }
+//         q1.push(element);
+//         while(q2.size()){
+//             q1.push(q2.front());
+//             q2.pop();
+//         }
+//     }
+
+//     int pop() {
+//         if(q1.size() == 0)
+//             return -1;
+//         int ans = q1.front();
+//         q1.pop();
+//         return ans;
+//     }
+
+//     int top() {
+//         return q1.size() ? q1.front() : -1;
+//     }
+// };
+
+// If we use single queue then at the time of appending we will first get the size of the queue and then append data to the queue. Now we will run loop of size 'queue_size' and push front element to queue and pop front element so now pushed element is at the front of the queue and order
+
 class Stack {
     private:
-    
-    // If we use single queue then at the time of appending we will first get the size of the queue and then append data to the queue. Now we will run loop of size 'queue_size' and push front element to queue and pop front element so now pushed element is at the front of the queue and order
-	queue<int> q1, q2;
+    queue<int> q1;
 
    public:
     Stack() {
         queue<int> empty;
-        q1 = empty;
-        q2 = empty;        
+        swap(q1, empty);       
     }
 
     /*----------------- Public Functions of Stack -----------------*/
@@ -27,18 +76,11 @@ class Stack {
     }
 
     void push(int element) {
-        if(q1.size() == 0){
-            q1.push(element);
-            return;
-        }
-        while(q1.size()){
-            q2.push(q1.front());
-            q1.pop();
-        }
+        int sz = q1.size();
         q1.push(element);
-        while(q2.size()){
-            q1.push(q2.front());
-            q2.pop();
+        for(int i = 0; i < sz; i++){
+            q1.push(q1.front());
+            q1.pop();
         }
     }
 
@@ -54,3 +96,4 @@ class Stack {
         return q1.size() ? q1.front() : -1;
     }
 };
+
