a3d8cb609c5d9530596762942fa2e5401688fee1
Mantra3265 <mantrarajgotecha@gmail.com>
2022-07-10 16:09:58 +0530

    Added Correct code for Median_of_two_sorted_arrays.cpp

--- Binary_Search/MustDoSecondTime/Median_of_two_sorted_arrays.cpp
+++ Binary_Search/MustDoSecondTime/Median_of_two_sorted_arrays.cpp
@@ -3,41 +3,43 @@
 
 #include <bits/stdc++.h>
 
-double median(vector<int> a, vector<int> b)
+double median(vector<int> nums1, vector<int> nums2)
 {
-    if(a.size() > b.size()) swap(a, b);
-    int n = a.size(), m = b.size();
-    int l = 0, h = n - 1, mid;
-    int midi = (m + n + 1) / 2; 
-    int tot = (m + n); 
-    // for handling last cases when one of the array gets over
-    a.push_back(INT_MAX);
-    b.push_back(INT_MAX);
-    while(l <= h){
-        mid = (l + h) >> 1;
-        int req = midi - mid - 1;
-        if(req < 0) {
-            h = mid - 1;
-            continue;
-        }
-        if(req == 0){
-            if(a[mid] <= b[0]){
-                if(tot & 1) return a[mid];
-                else return (a[mid] + b[0]) / 2.0;
+    int n = nums1.size(), m = nums2.size();
+        if(n > m)
+            return median(nums2, nums1);
+        nums1.push_back(INT_MAX);
+        nums2.push_back(INT_MAX);
+        int totM = (m + n + 1) / 2;
+        int tot = n + m;
+        if(n == 0)
+            return tot & 1 ? nums2[totM - 1] : (nums2[totM - 1] + nums2[totM]) / 2.0;
+        int l = 0, h = n - 1, mid;
+        while(l <= h){
+            mid = (l + h) >> 1;
+            int req = totM - 1 - mid;
+            if(req < 0){
+                h = mid - 1;
+                continue;
             }
-            else h = mid - 1;
-            continue;
+            if(req == 0){
+                if(nums1[mid] <= nums2[0]){
+                    if(tot & 1)
+                        return nums1[mid];
+                    return (nums1[mid] + min(nums1[mid + 1], nums2[0])) / 2.0;
+                }
+                h = mid - 1;
+                continue;
+            }
+            if(nums1[mid] <= nums2[req] and nums2[req - 1] <= nums1[mid + 1]){
+                if(tot & 1)
+                    return max(nums1[mid], nums2[req - 1]);
+                return (max(nums1[mid], nums2[req - 1]) + min(nums1[mid + 1], nums2[req])) / 2.0;
+            }
+            if(nums1[mid] > nums2[req])
+                h = mid - 1;
+            else 
+                l = mid + 1;
         }
-        int lastb = b[req - 1];
-        if(a[mid] <= b[req] and lastb <= a[mid + 1]){
-            if(tot & 1) return max(a[mid], lastb);
-            else return (max(a[mid], lastb) + min(a[mid + 1], b[req])) / 2.0;
-        }
-        else{
-            if(a[mid] > b[req]) h = mid - 1;
-            else l = mid + 1;
-        }
-    }
-    if(tot & 1) return b[midi - 1];
-    return (a[0] + b[midi - 1]) / 2.0;
+        return tot & 1 ? nums2[totM - 1] : (nums2[totM - 1] + min(nums1[0], nums2[totM])) / 2.0;
 }
